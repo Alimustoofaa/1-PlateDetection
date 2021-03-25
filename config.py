8dbcc65db8f1bb376d31b2ceb8f8a584de9b1c17
Alimustoofaa <softhouse.ali@gmail.com>
2021-03-25 23:50:34 +0700

    config path model

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+DIRECTORY_MODEL = 'models'
+
+detection_model = {
+    'filename': 'plate_detection.pb',
+    'url' : 'https://github.com/Alimustoofaa/wpod-license-plate/releases/download/v1.0/frozen_inference_graph.pb',
+    'file_size' : 19161802
+}
