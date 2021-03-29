6831081d94dd9f0c6a3199a437eead941caeb340
Ali Mustofa <softhouse.ali@gmail.com>
2021-03-29 13:23:11 +0700

    add area code

--- config.py
+++ config.py
@@ -7,3 +7,9 @@ detection_model = {
     'url' : 'https://github.com/Alimustoofaa/wpod-license-plate/releases/download/v1.0/frozen_inference_graph.pb',
     'file_size' : 19161802
 }
+
+area_code = {
+    'filename': 'kode_wilayah.json',
+    'url': 'https://github.com/Alimustoofaa/wpod-license-plate/releases/download/v1.0/kode_wilayah.json',
+    'file_size' : 121
+}
