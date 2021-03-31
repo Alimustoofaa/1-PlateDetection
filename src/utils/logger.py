05abab85116b2cfd208cbdfab130bf34d04952dc
Alimustoofaa <softhouse.ali@gmail.com>
2021-03-31 22:53:58 +0700

    add hour

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -16,6 +16,7 @@ class Logger(object):
         self.year            = str(datetime_now.year)
         self.month           = '0'+str(datetime_now.month) if len(str(datetime_now.month)) == 1 else str(datetime_now.month)
         self.day             = '0'+str(datetime_now.day) if len(str(datetime_now.day)) == 1 else str(datetime_now.day)
+        self.hour            = '0'+str(datetime_now.hour) if len(str(datetime_now.hour)) == 1 else str(datetime_now.hour)
         # return year, month, day
     
     def formater_time_log(self, *args):
@@ -24,7 +25,7 @@ class Logger(object):
     def path_log(self):
         path = f'logger/{self.year}/{self.month}'
         Path(path).mkdir(parents=True, exist_ok=True)
-        return  f'{path}/logging_{self.day}-{self.month}-{self.year}.log'
+        return  f'{path}/logging_{self.hour}_{self.day}-{self.month}-{self.year}.log'
 
     def logger(self):
         logging.Formatter.converter = self.formater_time_log
